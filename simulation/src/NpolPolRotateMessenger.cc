ec21a9f4f88cee45fa80386fcc3da247825502aa
tireman <tireman@nmu.edu>
2019-04-08 13:17:33 -0400

    Added a messenger class for the PolNucleonRotate class. This allows the turning on/off of the polarization scattering (n,p)/(n,C) code.

---
+++ simulation/src/NpolPolRotateMessenger.cc
@@ -0,0 +1,58 @@
+#include "NpolPolRotateMessenger.hh"
+#include "PolNucleonRotate.hh"
+
+#include "G4UIcommand.hh"
+#include "G4SystemOfUnits.hh"
+
+
+NpolPolRotateMessenger::NpolPolRotateMessenger(PolNucleonRotate* npol)
+:fnpolAction(npol){
+  fnpolDir = new G4UIdirectory("/npol/");
+  fnpolDir->SetGuidance("Npol general control commands.");
+
+  listCmd = new G4UIcmdWithoutParameter("/npol/list",this);
+  listCmd->SetGuidance("List of commands.");
+   
+  //help option
+  hlp = new G4UIcmdWithAString("/npol/help", this);
+  hlp->SetGuidance(" Lists possible actions to be taken");
+  hlp->SetGuidance(" Choice: h ");
+  hlp->SetParameterName("Choice", true);
+  hlp->SetDefaultValue("off");
+  hlp->SetCandidates("h off");
+  hlp->AvailableForStates(G4State_PreInit, G4State_Idle);
+  
+  polScatCmd = new G4UIcmdWithABool("/npol/polarScattering", this);
+  polScatCmd->SetGuidance(" Chose polarized (n,p) and (n,C) scattering with this bool" );
+  polScatCmd->SetGuidance(" Choice: true or false [default] ");
+  polScatCmd->SetParameterName("polarScatCmd", true);
+  polScatCmd->SetDefaultValue(false);
+
+  // Set defaults
+  fnpolAction->SetPolarScatteringValue(false);
+      
+}
+
+NpolPolRotateMessenger::~NpolPolRotateMessenger(){
+  
+  delete hlp;
+  delete listCmd;
+  delete polScatCmd; 
+  delete fnpolDir;
+}
+
+void NpolPolRotateMessenger::SetNewValue(G4UIcommand* command, G4String newVal){
+
+  
+  //G4cout << " \n  Is this GENERATOR being called at all??????????? \n \n";
+  if(command == hlp){
+    //florAction->SetUseHelp(newVal);
+    G4cout << "\nUsage: NpolGenerator [options] inputFile\n\n";
+    G4cout << "\toptions:\n";
+    G4cout << "\t/npol/help\t\tThis information\n";
+    G4cout << "\t/npol/rootFile [filename]\tdirect ROOT output to <filename>\n";
+  }
+  
+  if (polScatCmd){ fnpolAction->SetPolarScatteringValue(polScatCmd->GetNewBoolValue(newVal)); }
+  
+}
